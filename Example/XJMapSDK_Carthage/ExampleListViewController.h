b517d0f6a1d4c08ab839dd27ad47fa0039b355a6
zhangty <673534349@qq.com>
2018-12-26 15:30:23 +0800

    增加cartfile引用demo

---
+++ Example/XJMapSDK_Carthage/ExampleListViewController.h
@@ -0,0 +1,17 @@
+//
+//  ExampleListViewController.h
+//  XJMapSDK_Demo
+//
+//  Created by zhangty on 2018/12/25.
+//  Copyright © 2018 xunji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface ExampleListViewController : UIViewController
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ Example/XJMapSDK_Carthage/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  XJMapSDK_Carthage
+//
+//  Created by zhangty on 2018/12/25.
+//  Copyright © 2018 xunji. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
